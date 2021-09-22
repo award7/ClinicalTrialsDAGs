092d66df54c2f4c2780091ed29e765b656cb50c1
schragelab <schragelab@education.wisc.edu>
2021-09-22 04:38:58 -0700

    organization changes

--- dag_etl.py
+++ dag_etl.py
@@ -1,4 +1,5 @@
 from airflow import DAG
+from airflow.operators.python import PythonOperator
 # from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
 # # from airflow.providers.mongo.hooks.mongo import MongoHook
@@ -11,16 +12,34 @@ from airflow.utils.task_group import TaskGroup
 from datetime import datetime
 # import os
 # from docker.types import Mount
+import sys
+sys.path.append('/home/schragelab/repos/ClinicalTrialETL')
+from ClinicalTrialETL.etl.extract import extract_redcap_data
+from ClinicalTrialETL.redcap_api.api import Events, Forms
 
 
 with DAG('2019_0361_etl_dag', schedule_interval=None, start_date=datetime(2021, 8, 1), catchup=False) as dag:
     with TaskGroup(group_id='extract') as extract_tg:
+        TIMESTAMP = ""
+
+
+        # extract all data to store as a backup
+        extract_full = PythonOperator(
+            task_id='extract-test',
+            python_callable=extract_redcap_data
+        )
+
         extract_prescreening_data = DummyOperator(
             task_id='extract-prescreening-data'
         )
 
-        extract_screening_data = DummyOperator(
-            task_id='extract-screening-data'
+        events = [Events.screening_arm_1]
+        forms = [Forms.yogtt009_screening_visit_data_collection_form]
+
+        extract_screening_data = PythonOperator(
+            task_id='extract-screening-data',
+            python_callable=extract_redcap_data,
+            op_kwargs={'file_name': f'irb_2019_0361_{export_content}_raw_{timestamp}.{file_ext}'}
         )
 
         extract_cognitive_data = DummyOperator(
